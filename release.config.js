290fff109ebdb4934ceb876352397a7c00b5d454
yuvrajdahal <iamyuvraj456@gmail.com>
2025-03-09 18:21:16 +0545

    fix:fix desc

---
+++ release.config.js
@@ -0,0 +1,9 @@
+module.expots = {
+  branches: "main",
+  repositoryUrl: "https://github.com/yuvrajdahal/vite-action",
+  plugins: [
+    "@semantic-release/commit-analyzer",
+    "@semantic-release/release-notes-generator",
+    "@semantic-release/github",
+  ],
+};
